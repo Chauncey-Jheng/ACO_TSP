fd52fd46270966a90f1c75fd1e795e5350a83c36
zeta <g.y.tegridy@gmail.com>
2023-12-06 17:39:09 +0800

    experiment in various dataset

--- experiment.py
+++ experiment.py
@@ -29,13 +29,13 @@ def experiment_Chinese():
     plot.plot_path(x, y, path, "results\\Chinese_city_ant_constant_path.png")
     plot.plot_dist(distance_best, "results\\Chinese_city_ant_constant_distance.png")
 
-def experiment_serial(dataset:str, method:str, ant_count=100, alpha=1, beta=6, rho=0.2, Q=50, MAX_iter=200, use_CPUs=10):
+def experiment_serial(dataset:str, method:str, ant_count=100, alpha=1, beta=6, rho=0.2, Q=50, MAX_iter=200):
     # Get the data
     city_data = CityData()
     city_name, city_pos, distance_table = city_data.get_my_cities("datas\\"+dataset+".txt")
 
     # Load the model
-    aco = ACO(ant_count, alpha, beta, rho, Q, MAX_iter, use_CPUs)
+    aco = ACO(ant_count, alpha, beta, rho, Q, MAX_iter)
     aco.input_data(city_pos, distance_table)
     path_best, distance_best = aco.serial_iteration(method)
 
@@ -56,8 +56,38 @@ def experiment_serial(dataset:str, method:str, ant_count=100, alpha=1, beta=6, r
     x.append(x[0])
     y.append(y[0])
 
-    plot.plot_path(x, y, path, "results\\"+ dataset +"ant_"+ method + "_path_" + ".png")
-    plot.plot_dist(distance_best, "results\\"+ dataset +"ant_"+ method + "_distance_" +".png")
+    plot.plot_path(x, y, path, "results\\"+ dataset +"_ant_"+ method + "_path_" + ".png", method, dataset)
+    plot.plot_dist(distance_best, "results\\"+ dataset +"_ant_"+ method + "_distance_" +".png", method, dataset)
+
+def experiment_parallel(dataset:str, method:str, ant_count=100, alpha=1, beta=6, rho=0.2, Q=50, MAX_iter=200, use_CPUs=10):
+    # Get the data
+    city_data = CityData()
+    city_name, city_pos, distance_table = city_data.get_my_cities("datas\\"+dataset+".txt")
+
+    # Load the model
+    aco = ACO(ant_count, alpha, beta, rho, Q, MAX_iter, use_CPUs)
+    aco.input_data(city_pos, distance_table)
+    path_best, distance_best = aco.parallel_iteration(method)
+
+    # plot the result
+    path = [city_name[i] for i in path_best[-1]]
+    path.append(path[0])
+    print("The best path is", path)
+    print("The shortest distance is", distance_best[-1])
+
+    with open("results\\result.log", "a+") as f:
+        f.write("ant_"+ method + "_shortest_distance_" + dataset + " is "+ str(distance_best[-1]) + "\n")
+
+    x = []
+    y = []
+    for i in range(len(path_best[-1])):
+        x.append(city_pos[int(path_best[-1][i])][0])
+        y.append(city_pos[int(path_best[-1][i])][1])
+    x.append(x[0])
+    y.append(y[0])
+
+    plot.plot_path(x, y, path, "results\\"+ dataset +"_ant_"+ method + "_path_" + ".png", method, dataset)
+    plot.plot_dist(distance_best, "results\\"+ dataset +"_ant_"+ method + "_distance_" +".png", method, dataset)
 
 if __name__ == "__main__":
     # experiment_Chinese()
--- utils/plot.py
+++ utils/plot.py
@@ -2,26 +2,26 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.rcParams['font.family'] = 'STSong'
 
-def plot_path(x:list, y:list, path:list, savefig="results\\result_path.png"):
+def plot_path(x:list, y:list, path:list, savefig="results\\result_path.png", method="", dataset=""):
     '''
     Plot the best path.
     '''
     plt.figure()
     plt.plot(x, y, '-o')
-    plt.title("Best path graph")
+    plt.title(dataset +" " + method + " Best path graph")
     plt.xlabel("Longitude")
     plt.ylabel("Latitude")
     for i in range(len(path)):
         plt.annotate(path[i], xy=(x[i], y[i]), xytext=(x[i] + 0.3, y[i] + 0.3))
     plt.savefig(savefig)
     
-def plot_dist(distance:list, savefig="results\\result_dist.png"):
+def plot_dist(distance:list, savefig="results\\result_dist.png", method="", dataset=""):
     '''
     Plot the distance iteration condition.
     '''
     plt.figure()
     plt.plot(range(1, len(distance) + 1), distance)
-    plt.title("Distance iteration graph")
+    plt.title(dataset +" " + method + " Distance iteration graph")
     plt.xlabel("Number of iterations")
     plt.ylabel("Distance value")
     plt.savefig(savefig)
